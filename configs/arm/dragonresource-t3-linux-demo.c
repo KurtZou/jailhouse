fa9c7ecb4377c59acf6467cb14e4c5d9dfa97f59
kurtz <kurtzou@gmail.com>
2023-02-21 19:51:07 -0500

    change IVSHMEM range

--- configs/arm/dragonresource-t3-linux-demo.c
+++ configs/arm/dragonresource-t3-linux-demo.c
@@ -52,33 +52,33 @@ struct {
 	.mem_regions = {
 		/* IVSHMEM shared memory regions (demo) */
 		{
-			.phys_start = 0x4f6f0000,
-			.virt_start = 0x4f6f0000,
+			.phys_start = 0x6f6f0000,
+			.virt_start = 0x6f6f0000,
 			.size = 0x1000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_ROOTSHARED,
 		},
 		{
-			.phys_start = 0x4f6f1000,
-			.virt_start = 0x4f6f1000,
+			.phys_start = 0x6f6f1000,
+			.virt_start = 0x6f6f1000,
 			.size = 0x9000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_ROOTSHARED,
 		},
 		{
-			.phys_start = 0x4f6fa000,
-			.virt_start = 0x4f6fa000,
+			.phys_start = 0x6f6fa000,
+			.virt_start = 0x6f6fa000,
 			.size = 0x2000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_ROOTSHARED,
 		},
 		{
-			.phys_start = 0x4f6fc000,
-			.virt_start = 0x4f6fc000,
+			.phys_start = 0x6f6fc000,
+			.virt_start = 0x6f6fc000,
 			.size = 0x2000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_ROOTSHARED,
 		},
 		{
-			.phys_start = 0x4f6fe000,
-			.virt_start = 0x4f6fe000,
+			.phys_start = 0x6f6fe000,
+			.virt_start = 0x6f6fe000,
 			.size = 0x2000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_ROOTSHARED,
@@ -93,15 +93,15 @@ struct {
 				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_ROOTSHARED,
 		},
 		/* RAM */ {
-			.phys_start = 0x4f610000,
+			.phys_start = 0x6f610000,
 			.virt_start = 0,
 			.size = 0x10000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
 		},
 		/* RAM */ {
-			.phys_start = 0x48000000,
-			.virt_start = 0x48000000,
+			.phys_start = 0x68000000,
+			.virt_start = 0x68000000,
 			.size = 0x7600000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_DMA |
